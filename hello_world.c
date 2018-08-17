53c9a65c89119ae0afa71bbcfd437861f0561d8e
nafis nibir <nafisnibir@gmail.com>
2018-08-17 13:18:32 -0400

    echo

--- hello_world.c
+++ hello_world.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
 
+static char input[2048];
+
 int main(int argc, char** argv){
-  puts("Hello, Mexi!");
+
+  puts("Lispy Version 0.1");
+  puts("Press Ctrl+c to Exit\n");
+
+  while(1){
+    /* output prompt */
+    fputs("lispy> ", stdout);
+
+    /* read a line of user input max size 2048 */
+    fgets(input, 2048, stdin);
+
+    /* echo input */
+    printf("No you're a %s", input);
+  }
+
   return 0;
 }
